8cdd3c1aedcb7dfe9627245374f0e4aed250f3e5
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-23 23:55:42 -0500

    initial commit

---
+++ test/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    //$('form, p').css('fontSize', '30px');
+
+    //$("#list, .text").css('fontSize', '30px');
+
+   // $("input:text").css("background", "red")
+
+   // $("li:even").css("background","red")
+
+   //$("li:odd").css("background","red")
+
+   //$('.parent p:first-child').css("background", "red");
+
+   //$(".parent").find("#text").css("background", "red");
+
+   $(".parent").children("p").css("background","red");
+
+});
