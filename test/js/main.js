d1bdc2a83fb58d8b6100eadf60944a43d81e4d90
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-12-02 00:52:04 -0500

    initial commit

--- test/js/main.js
+++ test/js/main.js
@@ -14,6 +14,31 @@ $(document).ready(function(){
 
    //$(".parent").find("#text").css("background", "red");
 
-   $(".parent").children("p").css("background","red");
+   //$(".parent").children("p").css("background","red");
 
+   //$('.user').parent().css("background","red");
+
+   //$('.second').next().css("background", 'blue');
+
+   //$(".list li:eq(2)").css("background", "red");
+
+   //$(".list").parents().eq(1).css("background", "red");
+
+   //$('div').filter('.midd').css("background", "red");
+
+    // $('.parent').append('<div class="newDiv">Mi New Div</div>');
+
+   // $('<div class="newDiv">Mi New Div</div>').appendTo('.parent');
+
+    // $('.parent').prepend('<div class="newDiv">Mi New Div</div>');
+
+    //$('.element').before('<div>After</div>');
+
+   // $('.element').after('<div>After</div>');
+
+  // $('.replaceMe').replaceWith('<div>Replaced</div>');
+
+  //$('<div>Replace</div>').replaceAll('.replaceMe,.replaceMeAG');
+
+  $('.removeMe').remove();
 });
