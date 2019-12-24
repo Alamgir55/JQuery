46c3bbd15cde9456bf008b4ec6c2020ceb7e6bcd
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-12-24 00:35:40 -0500

    card widget

---
+++ card slider/js/main.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+
+    var itemLength = $('.cards_container .item').length;
+    var current = 1;
+    $('.total_slides').text(itemLength);
+
+    $('.cardSlider .btn_prev').on("click", function(){
+
+        if(current > 1){
+            current = current - 1;
+           showSlider(current);
+        }else{
+            current = itemLength;
+            showSlider(current);
+        }
+    });
+
+    $('.cardSlider .btn_next').on("click", function(){
+
+        if(current !== itemLength){
+            current = current + 1;
+            showSlider(current);
+        }else{
+            current = 1;
+            showSlider(current);
+        }
+    });    
+
+    function showSlider(current){
+        $('.cards_container .item').removeClass('active');
+        $('div[data-id="' + current + '"]').addClass('active');
+
+        $('.current_slide').text(current);
+    }
+
+});
