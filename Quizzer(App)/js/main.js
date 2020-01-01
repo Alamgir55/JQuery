972a0e32e988271553c510b8c3a41857cfc1ef99
Alamgir Hossain <alamgir555hossain@gmail.com>
2020-01-01 00:19:16 -0500

    Complete Project

--- Quizzer(App)/js/main.js
+++ Quizzer(App)/js/main.js
@@ -1,13 +1,20 @@
 
 $(document).ready(function(){
 
+
     (function startQuiz(){
+
+        this.settings = {
+            results: []
+        }
+
         this.loadQuiz = function(){
             $('.panel_one h1').show('drop',500,function(){
                 $('.start_quiz').addClass('started',500);
             })
             $('.start_quiz').on('click',function(){
                 showPanel(1);
+                listenNext();
             })
         };
 
@@ -16,8 +23,75 @@ $(document).ready(function(){
             current.find('.wrapper').animate({left:"-=100px",opacity:0},500,function(){
 
                 current.addClass('hidden');
+
+                var next = $('div[data-panel="'+ position +'"]');
+                next.removeClass('hidden');
+                showNext(next);
             });
-        }
+        };
+
+        this.showNext = function(next){
+            var wrapper = next.find('.wrapper');
+
+            wrapper.fadeIn('500', function(){
+                manageOptions(next)
+            });
+        };
+
+        this.manageOptions = function(next){
+            var options = next.find('.options');
+            var children = options.find('div');
+            var counter = 0;
+
+            children.each(function(i, el){
+                $(el).delay(counter).fadeIn(300);
+                counter += 500;
+            });
+
+            children.on('click', function(){
+                children.removeClass('active');
+                next.addClass('valid');
+                $(this).addClass('active');
+            });
+        };
+
+        this.listenNext = function(){
+            $('.next_question').on('click', function(){
+                var next = $(this).data('next');
+
+                if(validateSelection($(this))){
+                    showPanel(next);
+                    showProgressAndStore(next);
+                }
+            });
+        };
+
+        this.validateSelection = function($this){
+            var parent = $this.parents().eq(1);
+
+            if(parent.hasClass('valid')){
+                return true;
+            }else{
+                $('.error').fadeIn('300', function(){
+                    $(this).delay(1000).fadeOut('300')
+                })
+                return false;
+            }
+        };
+
+        this.showProgressAndStore = function(panel){
+            $('.progress .bar').animate({"width": "+=25%"}, 500);
+
+            var options = $('div[data-panel="'+(panel - 1)+'"]').find('.options');
+
+            options.find('div').each(function(i,el){
+                if($(this).hasClass('active')){
+                    settings.results.push($(this).text());
+                    console.log(settings.results);
+                }
+            })
+        };
+
         loadQuiz();
     })();
 
