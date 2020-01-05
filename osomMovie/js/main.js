db3c654d553633d273242cfaf2cfb37a0c1e02ac
Alamgir Hossain <alamgir555hossain@gmail.com>
2020-01-05 01:03:57 -0500

    initial commit

--- osomMovie/js/main.js
+++ osomMovie/js/main.js
@@ -1,8 +1,19 @@
 
 var osomMovie = {};
 
+osomMovie.database = [];
+
+osomMovie.loadAssets = function(){
+    $.getJSON("db/movies.json", function(data){
+        osomMovie.database = data;
+        osomMovie.init();
+    })
+}
+
 osomMovie.init = function(){
     osomMovie.filterSlider();
+    osomMovie.getType();
+    osomMovie.getDirector();
 }
 
 osomMovie.filterSlider = function(){
@@ -22,4 +33,27 @@ osomMovie.filterSlider = function(){
     });
 }
 
-osomMovie.init();
+
+osomMovie.getType = function(){
+    var types = [];
+    $.each(osomMovie.database, function(index, elem){
+        if($.inArray(osomMovie.database[index].type, types)){
+            var typeValue = osomMovie.database[index].type;
+            types.push(typeValue);
+            $('#categories').append('<option value="'+typeValue+'">'+typeValue+'</option>');
+        }
+    })
+}
+
+osomMovie.getDirector = function(){
+    var directors = [];
+    $.each(osomMovie.database, function(index, elem){
+        if($.inArray(osomMovie.database[index].director, directors)){
+            var directorValue = osomMovie.database[index].director;
+            directors.push(directorValue);
+            $('#directors').append('<option value="'+directorValue+'">'+directorValue+'</option>');
+        }
+    })
+}
+
+osomMovie.loadAssets();
