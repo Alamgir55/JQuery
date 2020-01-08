84cd590dec60549c4131fa4092714d567ab50450
Alamgir Hossain <alamgir555hossain@gmail.com>
2020-01-08 00:36:38 -0500

    complet Project

--- osomMovie/js/main.js
+++ osomMovie/js/main.js
@@ -14,6 +14,7 @@ osomMovie.init = function(){
     osomMovie.filterSlider();
     osomMovie.getType();
     osomMovie.getDirector();
+    osomMovie.generateMarkup();
 }
 
 osomMovie.filterSlider = function(){
@@ -53,6 +54,92 @@ osomMovie.getDirector = function(){
             directors.push(directorValue);
             $('#directors').append('<option value="'+directorValue+'">'+directorValue+'</option>');
         }
+    })
+}
+
+osomMovie.generateMarkup = function(){
+    var template = '';
+
+    $.each(osomMovie.database, function(index){
+        var db = osomMovie.database;
+
+        template += '<div class="movie_item" data-id='+db[index].id+'>';
+		template +=	'<div class="header">';
+		template +=		'<div class="left">';
+		template +=			'<img src="images/'+db[index].img+'" alt="">';
+		template +=		'</div>';
+		template +=		'<div class="right">';
+		template +=			'<h3>'+db[index].title+'</h3>';
+		template +=			'<div class="node">';
+		template +=				'<span>Year:</span> '+db[index].year+'';
+		template +=			'</div>';
+		template +=			'<div class="node">';
+		template +=				'<span>Director:</span>'+db[index].director;
+		template +=			'</div>';
+		template +=			'<div class="node">';
+		template +=				'<span>Type:</span>'+db[index].type;
+		template +=			'</div>';
+		template +=			'<div class="show_desc">See Description</div>';
+		template +=		'</div>';
+        
+		template +=		'<div class="description">';
+		template +=			'<strong>Description:</strong>'+db[index].desc;
+		template +=		'</div>';
+		template +=	'</div>';
+		template += '</div>';
+    });
+
+    $('.movies_content').append(template);
+    osomMovie.showDescription();
+    osomMovie.startFilter();
+}
+
+osomMovie.showDescription = function(){
+    $('.show_desc').on('click', function(){
+        var $this = $(this);
+        var parent = $(this).parents().eq(2);
+        var element = parent.find('.description');
+
+        element.slideToggle(300, function(){
+            if($this.hasClass('active')){
+                $this.text('See Description').removeClass('active');
+            }else{
+                $this.text('Hide Description').addClass('active');
+            }
+        })
+    })
+}
+
+osomMovie.startFilter = function(){
+    $('select').on('change', function(){
+        var db = osomMovie.database;
+        var type = $('#categories').val();
+        var director = $('#directors').val();
+        var results = [];
+
+        $.each(db, function(index){
+            if(db[index].type === type){
+                results.push(db[index].id);
+            }
+            if(db[index].director === director){
+                results.push(db[index].id)
+            }
+        });
+        
+        if(results.length < 1){
+            $('.movie_item').show();
+        }else{
+            var uniqueArray = [];
+
+            $.each(results, function(i, e){
+                if($.inArray(e,uniqueArray) == -1) uniqueArray.push(e);
+            })
+            $('.movie_item').hide();
+            $.each(uniqueArray,function(i,e){
+                $('div[data-id="'+e+'"]').show();
+            })
+        }
+
     })
 }
 
